a263e6501482bb8ae450ff35364fd27bd5f3336a
Oasis <64927775+oasis-sharp@users.noreply.github.com>
2022-01-11 09:46:18 +0000

    question 1 complete i scrapped q2 like 7 times though  it makes me want to cry

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            primeFactors(10);    
+        }
+
+        static int primeFactors(int i)
+        {
+            List<int> listOfFactors = new List<int>();
+
+            for (int x = 2; x <= i; x++)
+                if (i % x == 0 && x != 1)
+                {
+                    listOfFactors.Add(x);
+                    i = i / x;
+                    
+                }
+            
+            listOfFactors = listOfFactors.Distinct().ToList();
+            int num = 1;
+
+            for(i = 0; i < listOfFactors.Count; i++)
+            {
+                num *= listOfFactors[i];
+            }
+
+            return num;
+        }
+
+    }
+}
